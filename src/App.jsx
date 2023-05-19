cbdf05a89d60d57ee28659ddc59d8bfe69728c36
Frank Reyes <freyes081@gmail.com>
2023-05-19 09:10:03 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+
+
+function App() {
+const [data, setData] = useState(null);
+
+const fetchData = async () => {
+  const response = await fetch('https://api.chucknorris.io/jokes/random');
+  const data = await response.json();
+  setData(data);
+}
+useEffect(() => {
+  fetchData();
+ 
+}, [])
+
+
+
+
+  const handleClick = () => {
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(response => response.json())
+    .then(data => setData(data))
+    
+  }
+  
+  return (
+    <>
+      <div>{data?.value}</div>
+      <button onClick={handleClick}>Nuevo Chiste</button>
+    </>
+  )
+}
+
+export default App
